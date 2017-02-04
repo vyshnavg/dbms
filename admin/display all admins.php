2da55d12d1f81c4adde0824dc0496f2c266b7d5c
vyshnavgireesh <vyshnavxlr8@gmail.com>
2017-02-04 20:02:38 +0530

    display all admins complete

---
+++ admin/display all admins.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>PHP Site Michael Mazur</title>
+</head>
+<body>
+	<?php
+		include 'dbh.php';
+		$result = mysqli_query($conn,"SELECT admin_id,admin_password FROM admin")or die(mysqli_error($conn));
+		
+		echo "<table border='1'><tr><th>Admin ID</th><th>Admin Password</th></tr>";
+		
+		while($row = mysqli_fetch_array($result)){
+    		print "<tr><td>".$row['admin_id']."</td><td>".$row['admin_password']."</td></tr>";
+		}
+		print "</table>";
+		//mysql_close($con);
+	?>
+</body>
+</html>
